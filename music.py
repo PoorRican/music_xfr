1407e0895ee593f71c0e37d4424871307314e4d2
Josue Figueroa <poor.rican@pm.me>
2021-06-17 21:12:29 -0700

    Updated music models

--- music.py
+++ music.py
@@ -1,24 +1,61 @@
+import spotify.models as models
 from typing import List
 
 
-def Artist(object):
+class Artist(object):
     def __init__(self, name: str):
-        name = name
+        self.name = name
+
+    def __repr__(self):
+        return f"{self.name}"
+
+    @staticmethod
+    def from_spotify(artist: models.Artist):
+        return Artist(artist.name)
 
 
-def Album(object):
+class Album(object):
     def __init__(self, name: str, artist: Artist):
-        name = name
-        artist = artist
+        self.name = name
+        self.artist = artist
+
+    def __repr__(self):
+        return f"{self.name} by {self.artist}"
+
+    def from_spotify(album: models.Album):
+        name = album.name
+        artist = Artist.from_spotify(album.artists[0])
+
+        return Album(name, artist)
 
 
-def Track(object):
+class Track(object):
     def __init__(self, name: str, album: Album):
-        name = name
-        album = album
+        self.name = name
+        self.album = album
+
+    def __repr__(self):
+        return f"'{self.name}' in {self.album} by {self.album.artist}"
+
+    @staticmethod
+    def from_spotify(track: models.Track):
+        name = track.name
+        album = Album.from_spotify(track.album)
+
+        return Track(name, album)
 
 
-def Playlist(object):
+class Playlist(object):
     def __init__(self, name, tracks: List[Track]):
-        name = name
-        tracks = tracks
+        self.name = name
+        self.tracks = tracks
+
+    def __repr__(self):
+        return f"Playlist: '{self.name}' with {len(self.tracks)} tracks"
+
+    @staticmethod
+    def from_spotify(playlist: models.Playlist):
+        name = playlist.name
+        tracks = [Track.from_spotify(i) for i in playlist.get_all_tracks()]
+
+        return Playlist(name, tracks)
